4b9edee5c80409949bfd923e51bbc44269bc6fc9
gregdu91 <greg91400@hotmail.fr>
2020-11-05 17:23:55 +0100

    2 commit

--- HttpDemo/app/src/main/java/com/example/httpdemo/MainActivity.kt
+++ HttpDemo/app/src/main/java/com/example/httpdemo/MainActivity.kt
@@ -3,6 +3,11 @@ package com.example.httpdemo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import okhttp3.Callback
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import java.io.IOException
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,5 +19,19 @@ class MainActivity : AppCompatActivity() {
         TODO("not yet implemented")
 
         Log.i("HttpDemo", "Loading JSON")
+
+        var url= "https://my-json-server.typicode.com/johnrowley/demojson/posts"
+
+        Log.i("HTTPDemo", "url= $url")
+
+        val request = Request.Builder().url(url).build()
+
+        val client = OkHttpClient
+
+        client.newCall(request).enqueue(object : Callback) {
+
+        override fun onFailure(call: Call, e: IOException)
+        }
+
     }
 }
